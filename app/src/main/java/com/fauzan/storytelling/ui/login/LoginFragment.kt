c109cedb99e33f011066d253a52be4a71cf138eb
Fauzan Radji <72055502+fauzan-radji@users.noreply.github.com>
2023-10-26 00:30:19 +0800

    feat: register layout

--- app/src/main/java/com/fauzan/storytelling/ui/login/LoginFragment.kt
+++ app/src/main/java/com/fauzan/storytelling/ui/login/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.fauzan.storytelling.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -19,5 +20,14 @@ class LoginFragment : Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnRegister.setOnClickListener {
+            val toRegisterFragment = LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+            view.findNavController().navigate(toRegisterFragment)
+        }
     }
 }
